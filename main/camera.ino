bcf7959971047d1a7a6118d8412e511099b5ff5f
borgerpj <borgerpj@rose-hulman.edu>
2025-02-05 12:48:58 -0500

    Added new ground point calculation
    git commit -m "resolved camera merge conflict"

--- main/camera.ino
+++ main/camera.ino
@@ -17,8 +17,23 @@ const int wheelbase = 190;
 const int camera_height = 165;
 const double camera_dist_to_sensor = 50;
 const double camera_angle = 13 * M_PI / 60; // 39deg
-const double back_focal_length = 1.3; // BFL = 1.3mm, EFL = 4.6mm
+
+const double back_focal_length = 1.3;// BFL = 1.3mm, EFL = 4.6mm
+
+const float cam_z_min = 3.5; // all in inches
+const float cam_z_max = 16;
+const float cam_x_max = 6.1875;
+const float cam_x_min = 3.3125;
+
 // const double lookahead_distance = 500; // TODO: Set dynamically?
+
+float mapfloat(float value, float fromlow, float fromhigh, float tolow, float tohigh) {
+  value -= fromlow;
+  value *= (tohigh - tolow) / (fromhigh-fromlow);
+  value += tolow;
+
+  return value;
+}
 
 const Point3 rear_focal = {
   0,
@@ -39,13 +54,21 @@ Point3 from_pixel_coords(Point2 px) {
   return screen;
 }
 
-Point3 project_ground(Point3 screen) {
+Point3 project_ground(Point2 screen) {
   Point3 ground;
 
-  double scale = -screen.y / (screen.y - rear_focal.y);
-  ground.x = screen.x + scale * (screen.x - rear_focal.x);
-  ground.y = 0;
-  ground.z = screen.z + scale * (screen.z - rear_focal.z);
+
+  // double scale = -screen.y / (screen.y - rear_focal.y);
+  // ground.x = screen.x + scale * (screen.x - rear_focal.x);
+  // ground.y = 0;
+  // ground.z = screen.z + scale * (screen.z - rear_focal.z);
+
+
+  ground.z = mapfloat(screen.z, 0, PY, cam_z_min, cam_z_max);
+
+  float x_offset = cam_x_min + ((cam_z_max - cam_z_min)/(cam_x_max - cam_x_min)) (ground.z - cam_z_min);
+
+  ground.x = (screen.x, 0, 240, -x_offset, x_offset)
 
   return ground;
 }
@@ -116,13 +139,21 @@ bool Camera::read() {
           std::swap(target, origin);
         }
 
-        Point3 screen_target = from_pixel_coords(target);
-        Point3 screen_origin = from_pixel_coords(origin);
-        Serial.printf("Screen: from (%f, %f, %f) to (%f, %f, %f)\n", screen_origin.x, screen_origin.y, screen_origin.z, screen_target.x, screen_target.y, screen_target.z);
 
-        Point3 ground_target = project_ground(screen_target);
-        Point3 ground_origin = project_ground(screen_origin);
-        Serial.printf("Ground: from (%f, %f, %f) to (%f, %f, %f)\n", ground_origin.x, ground_origin.y, ground_origin.z, ground_target.x, ground_target.y, ground_target.z);
+        // Point3 screen_target = from_pixel_coords(target);
+        // Point3 screen_origin = from_pixel_coords(origin);
+        // Serial.printf("Screen: from (%f, %f, %f) to (%f, %f, %f)\n", screen_origin.x, screen_origin.y, screen_origin.z, screen_target.x, screen_target.y, screen_target.z);
+
+        // Point3 ground_target = project_ground(screen_target);
+        // Point3 ground_origin = project_ground(screen_origin);
+        // Serial.printf("Ground: from (%f, %f, %f) to (%f, %f, %f)\n", ground_origin.x, ground_origin.y, ground_origin.z, ground_target.x, ground_target.y, ground_target.z);
+
+        // Point3 screen_target = from_pixel_coords(target);
+        // Point3 screen_origin = from_pixel_coords(origin);
+
+        Point3 ground_target = project_ground(target);
+        Point3 ground_origin = project_ground(origin);
+
 
         this->angle = compute_angle(ground_target, ground_origin) * 180.0 / M_PI;
         this->steering_angle = compute_steering_angle(ground_target, (Point3) {0, 0, -wheelbase}) * 180.0 / M_PI;
@@ -131,3 +162,5 @@ bool Camera::read() {
       return false;
     }
 }
+
+
